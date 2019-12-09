f0cb3a8386f6d28296125655f5d3ce49e132ae0d
Mario Theuermann <mario.theuermann@student.tugraz.at>
2019-12-09 18:20:31 +0100

    change names; remove old/nonworking includeHTML script;

--- navbar.js
+++ navbar.js
@@ -1,22 +1,22 @@
 // Get the Sidebar
-var mySidebar = document.getElementById("mySidebar");
+var myNavbar = document.getElementById("myNavbar");
 
 // Get the DIV with overlay effect
 var overlayBg = document.getElementById("myOverlay");
 
 // Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-  if (mySidebar.style.display === 'block') {
-    mySidebar.style.display = 'none';
+function navbar_open() {
+  if (myNavbar.style.display === 'block') {
+    myNavbar.style.display = 'none';
     overlayBg.style.display = "none";
 } else {
-    mySidebar.style.display = 'block';
+    myNavbar.style.display = 'block';
     overlayBg.style.display = "block";
 }
 }
 
 // Close the sidebar with the close button
-function w3_close() {
-  mySidebar.style.display = "none";
+function navbar_close() {
+  myNavbar.style.display = "none";
   overlayBg.style.display = "none";
 }
