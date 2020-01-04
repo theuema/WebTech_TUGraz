581cdd243e47230e50d4b2a8f6ec3f5249e3c83c
Jannik Hildebrandt <jhildebrandt@student.tugraz.at>
2020-01-04 21:21:45 +0100

    started with rgb splitter

--- imageLoader.js
+++ imageLoader.js
@@ -1,18 +1,70 @@
+var imageSize = 200;
 var imageLoader = document.getElementById('imageLoader');
 imageLoader.addEventListener('change', handleImage, false);
 var canvas = document.getElementById('imageCanvas');
 var ctx = canvas.getContext('2d');
+
+var rgbSplitButton = document.getElementById('w3-bar-rgb');
+rgbSplitButton.addEventListener('click', splitRGB, false);
+var imgWidth, imgHeight, xPos = 350, yPos = 150;
 
 function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
         var img = new Image();
         img.onload = function(){
-            canvas.width = img.width;
+            /*canvas.width = img.width;
             canvas.height = img.height;
-            ctx.drawImage(img,0,0);
+            ctx.drawImage(img,0,0);*/
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            var ratio = img.width / img.height;
+            if(img.width > img.height) {
+                imgWidth = imageSize;
+                imgHeight = imageSize / ratio;
+            }
+            else {
+                imgWidth = imageSize * ratio;
+                imgHeight = imageSize;
+            }
+            ctx.drawImage(img,xPos,yPos,imgWidth,imgHeight);
         }
         img.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);     
+}
+
+function splitRGB() {
+    var imgd = ctx.getImageData(xPos, yPos, imgWidth, imgHeight);
+                    
+    var pix = imgd.data;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let original = [...pix];
+
+    //red
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        //This would be something that could be done by workers
+        pix[i] = original[i];
+        pix[i+1] = 0;
+        pix[i+2] = 0;
+    }
+    ctx.putImageData(imgd, xPos, yPos+imgHeight/2);
+
+    //green
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        //This would be something that could be done by workers
+        pix[i] = 0;
+        pix[i+1] = original[i+1];
+        pix[i+2] = 0;
+    }
+    ctx.putImageData(imgd, xPos-imgWidth/2, yPos-imgHeight/2);
+
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        //This would be something that could be done by workers
+        pix[i] = 0;
+        pix[i+1] = 0;
+        pix[i+2] = original[i+2];
+    }
+    ctx.putImageData(imgd, xPos+imgWidth/2, yPos-imgHeight/2);
 }
